30bffa67d473f400e3e3bbb1ee04b71457dedc2a
Culurio <claudiohcx@gmail.com>
2022-06-12 04:48:38 +0100

    sdsad

--- portfolio/models.py
+++ portfolio/models.py
@@ -34,7 +34,6 @@ class Project(models.Model):
 
     title = models.CharField(max_length=50,default='')
     description = models.CharField(max_length=500)
-    image = models.ImageField(upload_to='pictures/', blank=True)
     year = models.IntegerField(default=0)
     school_year = models.IntegerField(default=0)
     github = models.URLField()
@@ -86,7 +85,6 @@ class Post(models.Model):
     title = models.CharField(max_length = 255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
-    post_image = models.ImageField(upload_to='pictures/', blank=True)
     post_date = models.DateField(auto_now_add= True)
 
     def __str__(self):
